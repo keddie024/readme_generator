d937bc65618790db9370099349a7ba6083147332
Kevin Eddie <keddie024@gmail.com>
2021-04-16 19:22:35 -0400

    added init function. still to test

--- index.js
+++ index.js
@@ -54,12 +54,18 @@ const questions = [
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) { 
-    
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("Generating README...");
+        }
+    }
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer.prompt(questions).then()
+    inquirer.prompt(questions).then(writeToFile("README.md", generateMarkdown(response)));
 }
 
 // Function call to initialize app
